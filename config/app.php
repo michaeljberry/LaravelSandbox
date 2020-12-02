b892fc0828442fceb9692ad2c495abc37ecee0b8
Michael Berry <michael@saperemarketing.com>
2020-12-02 07:00:02 -0500

    Add Image service provider

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        MichaelBerry\MBImage\MBImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
