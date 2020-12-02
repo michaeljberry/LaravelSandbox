e33635a6733a19f7ebbb1749e6b2941ff54925c0
Michael Berry <michael@saperemarketing.com>
2020-12-02 07:04:30 -0500

    Add interface for image functions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link href="https://getbootstrap.com/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -61,10 +61,13 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .mt-50 {
+                margin-top: 50px;
+            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -93,6 +96,50 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+                <div class="container mt-50" style="text-align: left;">
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Upload Image</h5>
+                                    <form method="POST" action="{{ url('mbimage') }}" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label for="iamge-file">Image File</label>
+                                            <input type="file" class="form-control-file" id="iamge-file" name="image" />
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @if(!empty($images))
+                    <div class="row" style="padding-top: 20px">
+                        <div class="col-lg-12 text-center">
+                            <h1>Uploaded Image</h1>
+                        </div>
+                        <div class="col-lg-4 text-center">
+                            <img src="{{ $images->thumbnail_size }}" class="img-fluid" />
+                            <div class="text-center">
+                                <a href="{{ $images->thumbnail_size }}" class="btn btn-primary btn-sm" target="_blank">Thumbnail Size</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 text-center">
+                            <img src="{{ $images->small_size }}" class="img-fluid" />
+                            <div class="text-center">
+                                <a href="{{ $images->small_size }}" class="btn btn-primary btn-sm" target="_blank">Small Size</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 text-center">
+                            <img src="{{ $images->full_size }}" class="img-fluid" />
+                            <div class="text-center">
+                                <a href="{{ $images->full_size }}" class="btn btn-primary btn-sm" target="_blank">Full Size</a>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
